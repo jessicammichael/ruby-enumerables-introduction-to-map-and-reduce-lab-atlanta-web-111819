c34aaccde478b6f25f514bbf090f21899173c917
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 08:20:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,11 @@ map_to_no_change = ["paul", "gurney", "vladimir", "jessica", "chani"]
 map_to_double = [2, 4, 6, -18]
 
 map_to_square = [1, 4, 9, 81]
+
+
+reduce_to_total = [1,2,3]
+starting_point = [100]
+
+reduce_to_all_true = [1, 2, true, "razmatazz"]
+
+reduce_to_any_true = [1, 2, true, "razmatazz", false]
