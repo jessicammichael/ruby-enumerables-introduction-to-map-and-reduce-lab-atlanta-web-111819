3b5512cf15ad70ebeeed666583f34770fd363a3a
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 08:18:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,9 @@
 '''ruby
 
-# My Code here....
+map_to_negativize = [-1, -2, -3, 9]
+
+map_to_no_change = ["paul", "gurney", "vladimir", "jessica", "chani"]
+
+map_to_double = [2, 4, 6, -18]
+
+map_to_square = [1, 4, 9, 81]
